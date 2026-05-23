6c3d653064fa4bd9312ef86d25b06654ccc0a5e7
Daniel Fukuhara <new@xfinity.local>
2026-05-23 00:32:20 -0500

    Add PHP extensions for Vercel Composer install

--- scripts/vercel-install.sh
+++ scripts/vercel-install.sh
@@ -1,4 +1,14 @@
 #!/usr/bin/env sh
 set -eu
 
-dnf install -y php-cli php-common php-mbstring php-pdo php-sqlite3 php-xml
+dnf install -y \
+  php-cli \
+  php-common \
+  php-bcmath \
+  php-curl \
+  php-intl \
+  php-mbstring \
+  php-pdo \
+  php-process \
+  php-sqlite3 \
+  php-xml
